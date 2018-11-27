784c42f620f8e2c2dd9382251dffaa174a3319f5
אופיר לוי <ofirlly65@gmail.com>
2018-11-27 19:47:47 +0200

    finish link list

--- שיעור 11/linkedList/linkedList/Program.cs
+++ שיעור 11/linkedList/linkedList/Program.cs
@@ -21,7 +21,29 @@ namespace linkedList
             list1.deleteList();
             list1.insertLast(4);
             list1.printList();
-            list1.exists(4);
+            list1.insertLast(6);
+            list1.printList();
+            list1.insertLast(7);
+            list1.printList();
+            Console.WriteLine(list1.exists(4));
+            Console.WriteLine(list1.exists(6));
+            Console.WriteLine(list1.exists(100));
+            list1.insertAfter(list1.findNode(6), 5);
+            list1.printList();
+            list1.removeFirst();
+            list1.printList();
+            list1.remove(20);
+            list1.printList();
+            list1.remove(5);
+            list1.printList();
+            Console.WriteLine("SORTED LINKLIST!!!! :");
+            linkedList list2 = new linkedList(10);
+            list2.addSorted(5);
+            list2.addSorted(12);
+            list2.addSorted(3);
+            list2.printList();
+
+
         }
 
         class node
@@ -61,11 +83,30 @@ namespace linkedList
             }
             public void insertAfter(node temp, int value)
             {
-
+                node curr = new node(value);
+                curr.next = temp.next;
+                temp.next = curr;
             }
             public void addSorted(int valueToAdd)
             {
-
+                node NEW = new node(valueToAdd);
+                node prev = null;
+                node temp = head;
+                while((temp!=null)&&(valueToAdd>temp.value))
+                {
+                    prev = temp;
+                    temp = temp.next;
+                }
+                if(prev==null)
+                {
+                    NEW.next = temp;
+                    head = NEW;
+                }
+                else
+                {
+                    NEW.next = temp;
+                    prev.next = NEW;
+                }
             }
             public void deleteList()
             {
@@ -76,10 +117,11 @@ namespace linkedList
                 for (node temp = head; temp != null; temp = temp.next)
                 {
                     if (temp.value == valueToFind)
+                    {
                         return true;
-                    else
-                        return false;
+                    }
                 }
+                return false;
             }
             public node findNode(int valueToFind)
             {
@@ -87,16 +129,34 @@ namespace linkedList
                 {
                     if (temp.value == valueToFind)
                         return temp;
-                    else
-                        return null;
                 }
+                return null;
             }
             public void removeFirst()
             {
-
+                if (head != null)
+                {
+                    node temp = head.next;
+                    head = temp;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                    return;
+                }
             }
             public void remove(int valueToRemove)
             {
+                node prev=head;
+                for (node temp= head; temp != null; prev = temp, temp = temp.next)
+                {
+                    if (temp.value == valueToRemove)
+                    {
+                        prev.next = temp.next;
+                        return;
+                    }
+                }
+                Console.WriteLine("there is no such value in this linklist");
 
             }
             public void printList()
