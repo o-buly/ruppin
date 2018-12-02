dfda9cd282a63d02fea4c25f13cd87b818ac9a79
אופיר לוי <ofirlly65@gmail.com>
2018-12-02 18:25:47 +0200

    putting the class in another tag

---
+++ שיעור 12/properties/properties/MAIN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Student p1 = new Student();
+            p1.age = 14;
+            p1.id = 305253932;
+            p1.name = "ofir";
+            p1.print();
+        }
+    }
+}
+
--- שיעור 12/properties/properties/Student.cs
+++ שיעור 12/properties/properties/Student.cs
@@ -6,17 +6,6 @@ using System.Threading.Tasks;
 
 namespace properties
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Student p1 = new Student();
-            p1.age = 14;
-            p1.id = 305253932;
-            p1.name = "ofir";
-            p1.print();
-        }
-    }
     class Student
     {
         public int id;
@@ -60,6 +49,4 @@ namespace properties
             Console.WriteLine($"{Id}\n{Name}\n{Age}");
         }
     }
-
 }
-
