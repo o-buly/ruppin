dd1d6e6d2ac9d8ff394b040dfd5270898c39297f
אופיר לוי <ofirlly65@gmail.com>
2018-12-11 12:51:12 +0200

    binary search + sort selection added

---
+++ שיעור 14/Binary_search/Binary_search/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 5, 7, 8, 11, 15, 18, 24, 25 };
+            int index = Binary_Search(nums, 11);
+            Console.WriteLine(index);
+            foreach (var num in nums)
+            {
+                Console.WriteLine(Binary_Search(nums, num));
+            }
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            Console.WriteLine(rnd.Next(0, 100));
+
+            nums = new int[2000];
+            for (int i = 0; i < 2000; i++)
+            {
+                nums[i] = i;
+            }
+            Array.Sort(nums);
+            Binary_Search(nums, 4000);
+
+
+        }
+        private static int Binary_Search(int[] nums, int valueToSearch)
+        {
+            int counter = 0;
+            for (int start = 0, end = nums.Length - 1; start <= end;)
+            {
+                int middleIndex = (start + end) / 2;
+                counter++;
+                if (nums[middleIndex] == valueToSearch)
+                {
+                    Console.WriteLine("counter= " + counter);
+                    return middleIndex;
+                }
+                else if (valueToSearch < nums[middleIndex])
+                    end = middleIndex - 1;
+                else
+                    start = middleIndex + 1;
+            }
+            Console.WriteLine("counter= " + counter);
+            return -1;
+        }
+    }
+}
---
+++ שיעור 14/sort_selection/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort_selection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // swap XOR
+            int num1 = 7;
+            int num2 = 5;
+            num2 ^= num1;
+            num1 = num2 ^ num1;
+            num2 ^= num1;
+
+            //swat +-
+            num1 = 5;
+            num2 = 7;
+            num1 -= num2;//num1=2;
+            num2 += num1;
+            num1 = num2 - num1;
+
+            int[] nums = new int[] { 3, 7, 2, 9, 1, 22, 5, 4 };
+            InsertionSort(nums);
+
+            foreach (var item in nums)
+            {
+                Console.Write(item+", ");
+            }
+            Console.WriteLine();
+        }
+        private static void InsertionSort(int [] nums)
+        {
+            int counter = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    counter++;
+                    if (nums[j] < nums[i])
+                    {
+                        //swap +- without temp
+                        nums[j] -= nums[i];
+                        nums[i] += nums[j];
+                        nums[j] = nums[i] - nums[j];
+                    }
+                }
+            }
+            Console.WriteLine("counter= "+counter);
+        }
+    }
+}
